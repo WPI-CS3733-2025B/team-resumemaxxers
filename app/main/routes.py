059378246a96006310148dbec22a62b830f1abee
Xunn3714 <zchen18@wpi.edu>
2025-11-18 19:13:37 -0500

    fix refresh errors

--- app/main/routes.py
+++ app/main/routes.py
@@ -46,9 +46,9 @@ def index():
         if form.course_instructors.data:
             Positions = Positions.join(Position.faculty).where(Faculty.id == form.course_instructors.data)
         if form.research_topics.data:
-            Positions = Positions.join(Position.research_topics).where(ResearchTopic.id == form.research_topics.data)
+            Positions = Positions.join(Position.research_topics).where(ResearchTopic.name == form.research_topics.data)
         if form.languages.data:
-            Positions = Positions.join(Position.languages).where(Language.id == form.languages.data)
+            Positions = Positions.join(Position.languages).where(Language.name == form.languages.data)
 
     Students = db.session.scalars(sqla.select(Student))
     PositionsA = db.session.scalars(Positions).all()
