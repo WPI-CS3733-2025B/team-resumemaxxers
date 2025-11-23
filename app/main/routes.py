c37480b10f92f54f0c23a033d8966e44e2dab696
Xunn3714 <zchen18@wpi.edu>
2025-11-23 16:31:46 -0500

    fixed the main index routes

--- app/main/routes.py
+++ app/main/routes.py
@@ -51,7 +51,13 @@ def index():
             Positions = Positions.join(Position.languages).where(Language.name == form.languages.data)
 
     Students = db.session.scalars(sqla.select(Student))
+    Faculties = db.session.scalars(sqla.select(Faculty))
     PositionsA = db.session.scalars(Positions).all()
+    if current_user.is_authenticated:
+        if current_user.role == 'faculty':
+            return redirect(url_for('faculty.faculty_index', faculty_id=current_user.id))
+        elif current_user.role == 'student':
+            return redirect(url_for('student.student_index', student_id=current_user.id))
     return render_template('student_index.html', title="Course List", students = Students, form = form, positions=PositionsA)
 
 @main.route('/faculty', methods=['GET'])
